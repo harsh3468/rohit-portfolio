c821d67ebe9873c7d5e1a8aef8ade810a48e064a
aarushirai2210 <aarushirai_2000@outlook.com>
2021-12-01 16:39:56 +0530

    first commit

--- components/Loading.js
+++ components/Loading.js
@@ -1,21 +1,22 @@
 import React from "react";
-import styles from "../styles//Loading.module.css";
+// import styles from "../styles//Loading.module.css";
 
 function Loading(props) {
   return (
-    <div className={props.loading ? styles.body_loading : styles.none}>
-    <style jsx>{`
+    // <div className={props.loading ? styles.body_loading : styles.none}>
+    // {/* <style jsx>{`
         
-      `}</style>
-      <div
-        className={styles.lds_ellipsis}
-      >
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
+    //   `}</style>
+    //   <div
+    //     className={styles.lds_ellipsis}
+    //   >
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //     <div></div>
+    //   </div> */}
+    // </div>
+    <div></div>
   );
 }
 
